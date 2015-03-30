b3947b5af43b4fb77e7e836decbc9fd8fbb62589
kolesnikovakate <kolesnikovakate@gmail.com>
2015-03-30 22:50:29 +0300

    Урок 13, Core Motion

--- Bouncer/ViewController.swift
+++ Bouncer/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var bounser = BouncerBehavior()
+
+    lazy var animator: UIDynamicAnimator = { UIDynamicAnimator(referenceView: self.view) }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        animator.addBehavior(bounser)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if (redBlock == nil) {
+            redBlock = addBlock()
+            redBlock?.backgroundColor = UIColor.redColor()
+            bounser.addBlock(redBlock!)
+        }
+
+        let manager = AppDelegate.Motion.Manager
+        if manager.deviceMotionAvailable {
+            manager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue()) { (data, error) -> Void  in
+                    self.bounser.gravity.gravityDirection = CGVector(dx: data.acceleration.x, dy: -data.acceleration.y)
+            }
+        }
+
     }
 
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        AppDelegate.Motion.Manager.stopAccelerometerUpdates()
+    }
 
+    var redBlock: UIView?
+
+    struct Constants {
+        static let BlockSize = CGSize(width: 40, height: 40)
+    }
+
+    func addBlock() -> UIView {
+        let block = UIView(frame: CGRect(origin: CGPoint.zeroPoint, size: Constants.BlockSize))
+        block.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
+        view.addSubview(block)
+        return block
+    }
 }
 
